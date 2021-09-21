ef18d17a4efab59e8218dae9711c66427f300fbe
Diciembre2021+ <jeisontrivino0617@gmail.com>
2021-09-21 15:41:17 -0500

    Entidades

--- SeguimientoNutricional.App/SeguimientoNutricional.App.Dominio/Paciente.cs
+++ SeguimientoNutricional.App/SeguimientoNutricional.App.Dominio/Paciente.cs
@@ -9,13 +9,18 @@ namespace SeguimientoNutricional.App.Dominio
         public int IdPaciente { get; set; }
         public varchar Contraseña { get; set;}
 
-
+        
         /// Relacion Entre Paciente y Coach
         public Coach CoachAsignadoPaciente { get; set;}
+
         /// Relacion Entre Paciente y Nutricionista
-        public Nutricionista NutricionistaAsignadoPaciente { get; set;}        
+        public Nutricionista NutricionistaAsignadoPaciente { get; set;}
+
+        
         /// Relacion Entre Paciente y HistoricoDatos
-        public HistoricoDatos HistoricoDatosPaciente { get; set;}        
+        public HistoricoDatos HistoricoDatosPaciente { get; set;}
+
+        
         /// Relacion Entre Paciente y SeguimientoNutricional
         public SeguimientoNutricional SeguimientoNutricionalPaciente { get; set;}
 
