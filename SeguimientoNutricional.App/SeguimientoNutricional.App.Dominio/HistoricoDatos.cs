7e9ca89ccac6ad3da43860883c78234673d93257
BarbaraJulieth <90597893+BarbaraJulieth@users.noreply.github.com>
2021-09-25 17:52:20 -0500

    Update HistoricoDatos.cs

--- SeguimientoNutricional.App/SeguimientoNutricional.App.Dominio/HistoricoDatos.cs
+++ SeguimientoNutricional.App/SeguimientoNutricional.App.Dominio/HistoricoDatos.cs
@@ -8,11 +8,11 @@ namespace SeguimientoNutricional.App.Dominio
         public char IdRegistro { get; set; }
         public char IdPaciente { get; set; }
         
-        /// Relacion entre HistoricoDatos e SeguimientoNutricional
-        public SeguimientoNutricional Seguimiento { get; set; }
+        /// Relacion entre HistoricoDatos e SeguimientoNutri
+        public SeguimientoNutri SeguimientoNutricionalPaciente { get; set; }
         /// Relacion entre HistoricoDatos y RegistroDatos
         public RegistroDatos Registro { get; set; }
         /// Relacion entre HistoricoDatos e HistoricoBasico
-        public HistoricoBasico HistoricoB { get; set; }
+        public HistoricoBasico HistoricoBasicoPaciente { get; set; }
     }
 }
