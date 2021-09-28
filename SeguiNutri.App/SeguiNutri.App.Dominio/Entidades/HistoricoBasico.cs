8ad897f19388a58d423ad191c49c92a19320b24c
Diciembre2021+ <jeisontrivino0617@gmail.com>
2021-09-28 10:48:04 -0500

    Movimiento de clases de capas

---
+++ SeguiNutri.App/SeguiNutri.App.Dominio/Entidades/HistoricoBasico.cs
@@ -0,0 +1,14 @@
+using system;
+namespace SeguimientoNutricional.App.Dominio
+{    
+
+    public class HistoricoBasico
+    {
+        
+        public char IdPaciente { get; set; }
+        
+        
+        
+        
+    }
+}
