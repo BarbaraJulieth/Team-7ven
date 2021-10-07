ba82a9f9fd34685fded334de37e6c3a58db9af6a
BarbaraJulieth <barbaratv2000@gmail.com>
2021-10-06 20:38:24 -0500

    Backend Registrar Paciente

---
+++ SeguiNutri.App/SeguiNutri.App.Frontend/Pages/Pacientes/Registrar.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SeguiNutri.App.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SeguiNutri.App.Dominio;
+ 
+namespace SeguiNutri.App.Frontend.Pages.Pacientes
+{
+    public class RegistrarModel : PageModel
+    {
+        private readonly IRepositorioPaciente _repoPaciente;
+ 
+        public Paciente Paciente {get;set;}
+ 
+        public RegistrarModel(IRepositorioPaciente _repoPaciente)
+        {
+            this._repoPaciente=_repoPaciente;
+        }
+ 
+        public void OnGet()
+        {
+            Paciente = new Paciente();
+        }
+ 
+        public IActionResult OnPost(Paciente paciente)
+        {
+            _repoPaciente.AddPaciente(paciente);
+            return RedirectToPage("Intro");
+        }
+ 
+    }
+}
+
