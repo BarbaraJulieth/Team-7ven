e6b430047fd0e4bd81ae738f7586d8dd07cb95d3
Diciembre2021+ <jeisontrivino0617@gmail.com>
2021-10-14 14:04:57 -0500

    Reppositorio Complilado Segui Nutri

---
+++ SeguiNutri.App/SeguiNutri.App.Persistencia/AppRepositorios/IRepositorioSeguimientoNutricional.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using SeguiNutri.App.Dominio;
+using System;
+
+namespace SeguiNutri.App.Persistencia.AppRepositorios
+{
+    public interface IRepositorioSeguimientoNutricional
+    {
+        IEnumerable<SeguimientoNutris> GetAllSeguimientoNutris(int IdPaciente);
+        SeguimientoNutris AddSeguimientoNutris(SeguimientoNutris seguimientoNutris);
+        SeguimientoNutris UpdateSeguimientoNutris(SeguimientoNutris seguimientoNutris);
+        void DeleteSeguimientoNutris(int Id);
+        SeguimientoNutris GetSeguimientoNutris(int IdPaciente);
+    }
+}
---
+++ SeguiNutri.App/SeguiNutri.App.Persistencia/AppRepositorios/RepositorioSeguimientoNutricional.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using SeguiNutri.App.Dominio;
+using System;
+
+namespace SeguiNutri.App.Persistencia.AppRepositorios
+{
+    public class RepositorioSeguimientoNutricional : IRepositorioSeguimientoNutricional
+    {
+        private readonly AppContext _appContext = new AppContext();
+
+        SeguimientoNutris IRepositorioSeguimientoNutricional.AddSeguimientoNutris(SeguimientoNutris seguimientoNutris)
+        {
+            var SeguimientoNutrisAgregado = _appContext.SeguimientosNutri.Add(seguimientoNutris);
+            _appContext.SaveChanges();
+            return SeguimientoNutrisAgregado.Entity;
+        }
+
+        void IRepositorioSeguimientoNutricional.DeleteSeguimientoNutris(int Id)
+        {
+            var SeguimientoNutrisAgregado = _appContext.SeguimientosNutri.FirstOrDefault(p => p.Id == Id);
+            if(SeguimientoNutrisAgregado == null)
+                return;
+            _appContext.SeguimientosNutri.Remove(SeguimientoNutrisAgregado);
+            _appContext.SaveChanges();    
+        }
+
+        IEnumerable<SeguimientoNutris> IRepositorioSeguimientoNutricional.GetAllSeguimientoNutris(int IdPaciente)
+        {
+            var lista = _appContext.SeguimientosNutri.Where(p => p.IdPaciente == IdPaciente);
+            return lista;
+        }
+
+        SeguimientoNutris IRepositorioSeguimientoNutricional.GetSeguimientoNutris(int IdPaciente)
+        {
+            var SeguimientoNutrisAgregado = _appContext.SeguimientosNutri.FirstOrDefault(p => p.IdPaciente == IdPaciente);
+            return SeguimientoNutrisAgregado;
+        }
+
+        SeguimientoNutris IRepositorioSeguimientoNutricional.UpdateSeguimientoNutris(SeguimientoNutris seguimientoNutris)
+        {   
+            var SeguimientoNutrisEncontrado = _appContext.SeguimientosNutri.FirstOrDefault(p => p.Id == seguimientoNutris.Id);
+            if(SeguimientoNutrisEncontrado!=null)
+            {
+                SeguimientoNutrisEncontrado.Id = seguimientoNutris.Id;
+                SeguimientoNutrisEncontrado.IdNutricionista = seguimientoNutris.IdNutricionista;
+                SeguimientoNutrisEncontrado.IdCoach = seguimientoNutris.IdCoach;
+                SeguimientoNutrisEncontrado.IdPaciente = seguimientoNutris.IdPaciente;
+                SeguimientoNutrisEncontrado.SugerenciasCuid = seguimientoNutris.SugerenciasCuid;
+
+                SeguimientoNutrisEncontrado.HistoricoBasicoPaciente = seguimientoNutris.HistoricoBasicoPaciente;
+                _appContext.SaveChanges();
+            }
+            return SeguimientoNutrisEncontrado;
+        }       
+    }
+}
